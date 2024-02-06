105c887eb17d715c4b87c11bd8f9d00572762e4f
Prachai <pradeepmeti.c@gmail.com>
2024-02-06 09:15:25 +0530

    13 Functional + 8

--- tests/Functional_HTML.spec.js
+++ tests/Functional_HTML.spec.js
@@ -26,82 +26,79 @@ const setup=(page)=>{
     loginpage:new LoginPage(page)
 }}
 ///////////////////////////////////////////////////////////////////////////////
-// test("_01_Home Page validation: New Chat, search textfield", async({page})=>{
-//     const {homepage}=setup(page)
-//     const input='How are these trends influencing global energy policies?'
+test("_01_Home Page validation: New Chat, search textfield", async({page})=>{
+    const {homepage}=setup(page)
+    const input='How are these trends influencing global energy policies?'
 
-//     await homepage.newChatHovering() // mouse hovering
-//     await homepage.searchTextField_(input)
-//     await homepage.searchResults() 
-// })
-// test("_02_Three vertical dot: Resource dropdown",async({page})=>{
-//     const {homepage}=setup(page)
+    await homepage.newChatHovering() // mouse hovering
+    await homepage.searchTextField_(input)
+    await homepage.searchResults() 
+})
+test("_02_Three vertical dot: Resource dropdown",async({page})=>{
+    const {homepage}=setup(page)
     
-//     await homepage.three_vertical_dot_()
-//     await homepage.resourceDropdown10_()
-//     await homepage.resourceDropdown20_()
-//     await homepage.resourceDropdown30_()
-//     await homepage.resourceDropdown40_()
-//     await homepage.resourceDropdown50_()
-//     await homepage.closeButton_()
-
-// })
-// test("_03_Three vertical dot: Creativity: Dropdown",async({page})=>{
-//     const {homepage}=setup(page)
-//     const input='How are these trends influencing global energy policies?'
+    await homepage.three_vertical_dot_()
+    await homepage.resourceDropdown10_()
+    await homepage.resourceDropdown20_()
+    await homepage.resourceDropdown30_()
+    await homepage.resourceDropdown40_()
+    await homepage.resourceDropdown50_()
+    await homepage.closeButton_()
+})
+test("_03_Three vertical dot: Creativity: Dropdown",async({page})=>{
+    const {homepage}=setup(page)
+    const input='How are these trends influencing global energy policies?'
     
-//     await homepage.three_vertical_dot_()
-//     await homepage.CreativityHigh_()
-//     await homepage.CreativityLow_()
-//     await homepage.CreativityMedium_()
-//     await homepage.closeButton_()
-// })
-// test("_04_Three vertical dot: History,Report,logout",async({page})=>{
-//     const {homepage,historyPage}=setup(page)
+    await homepage.three_vertical_dot_()
+    await homepage.CreativityHigh_()
+    await homepage.CreativityLow_()
+    await homepage.CreativityMedium_()
+    await homepage.closeButton_()
+})
+test("_04_Three vertical dot: History,Report,logout",async({page})=>{
+    const {homepage,historyPage}=setup(page)
 
-//     await homepage.three_vertical_dot_()
-//     await homepage.historyPage_()
-//     await historyPage.closeButton()
-//     await homepage.three_vertical_dot_()
-//     await homepage.reportPage_()
-//     await historyPage.closeButton()
-//     await homepage.three_vertical_dot_()
-//     await homepage.logoutButton_()
-// })
-// test("_05_History : Existing results, Delete the first result ", async({page})=>{
-//     const {homepage, historyPage}=setup(page)
-//     const searchInput='How are these trends influencing global energy policies?'
+    await homepage.three_vertical_dot_()
+    await homepage.historyPage_()
+    await historyPage.closeButton()
+    await homepage.three_vertical_dot_()
+    await homepage.reportPage_()
+    await historyPage.closeButton()
+    await homepage.three_vertical_dot_()
+    await homepage.logoutButton_()
+})
+test("_05_History : Existing results, Delete the first result ", async({page})=>{
+    const {homepage, historyPage}=setup(page)
+    const searchInput='How are these trends influencing global energy policies?'
 
-//     await homepage.three_vertical_dot_()
-//     await homepage.historyPage_()
-//     await historyPage.removeFirstSearchHistoryEntry(searchInput)
-// })
-// test("_06_Combine the results by selecting N check boxes",async({page})=>{
-//     const {homepage,historyPage}=setup(page)
-//     const checkboxCount=3;
+    await homepage.three_vertical_dot_()
+    await homepage.historyPage_()
+    await historyPage.removeFirstSearchHistoryEntry(searchInput)
+})
+test("_06_Combine the results by selecting N check boxes",async({page})=>{
+    const {homepage,historyPage}=setup(page)
+    const checkboxCount=3;
     
-//     await homepage.three_vertical_dot_()
-//     await homepage.historyPage_()
-//     await historyPage.threeverticalDot_historyPage()
-//     await historyPage.combinechats_()
-//     await historyPage.selectChats_(checkboxCount)
-//     await historyPage.selectCombinedChats()
-//     await historyPage.closeButton()
-// })
-
-// test("_07_Generate Report with Selected Content and Resources (N Checkboxes)",async({page})=>{
-//     const {homepage,historyPage}=setup(page)
-//     const checkboxCount=2;
+    await homepage.three_vertical_dot_()
+    await homepage.historyPage_()
+    await historyPage.threeverticalDot_historyPage()
+    await historyPage.combinechats_()
+    await historyPage.selectChats_(checkboxCount)
+    await historyPage.selectCombinedChats()
+    await historyPage.closeButton()
+})
+test("_07_Generate Report with Selected Content and Resources (N Checkboxes)",async({page})=>{
+    const {homepage,historyPage}=setup(page)
+    const checkboxCount=2;
     
-//     await homepage.three_vertical_dot_()
-//     await homepage.historyPage_()
-//     await historyPage.threeverticalDot_historyPage()
-//     await historyPage.genaratereport_3VerticalDot_()
-//     await historyPage.selectChats_(checkboxCount)
-//     await historyPage.genaratereport_colored_()
-//     await historyPage.closeButton()
-// })
-
+    await homepage.three_vertical_dot_()
+    await homepage.historyPage_()
+    await historyPage.threeverticalDot_historyPage()
+    await historyPage.genaratereport_3VerticalDot_()
+    await historyPage.selectChats_(checkboxCount)
+    await historyPage.genaratereport_colored_()
+    await historyPage.closeButton()
+})
 test("_08_Generate Report with Selected Contents (N Checkboxes)",async({page})=>{
     const {homepage,historyPage}=setup(page)
     const checkboxCount=2;
@@ -115,60 +112,59 @@ test("_08_Generate Report with Selected Contents (N Checkboxes)",async({page})=>
     await historyPage.genaratereport_colored_()
     await historyPage.closeButton()
 })
-// test("_09_Generate Report with Selected Resources (N Checkboxes)",async({page})=>{
-//     const {homepage, historyPage}=setup(page)
-//     const checkboxCount=2;
+test("_09_Generate Report with Selected Resources (N Checkboxes)",async({page})=>{
+    const {homepage, historyPage}=setup(page)
+    const checkboxCount=2;
     
-//     await homepage.three_vertical_dot_()
-//     await homepage.historyPage_()
-//     await historyPage.threeverticalDot_historyPage()
-//     await historyPage.genaratereport_3VerticalDot_()
-//     await historyPage.selectChats_(checkboxCount)
-//     await historyPage.resourcesButton()
-//     await historyPage.genaratereport_colored_()
-//     await historyPage.closeButton()
-// })
-// test("_10_Edit Report Title and Verify Reflection on Reports Page",async({page})=>{
-//     const {homepage,reportpage}=setup(page)
-//     const input="Q1: How are these trends influencing global energy policies?"
+    await homepage.three_vertical_dot_()
+    await homepage.historyPage_()
+    await historyPage.threeverticalDot_historyPage()
+    await historyPage.genaratereport_3VerticalDot_()
+    await historyPage.selectChats_(checkboxCount)
+    await historyPage.resourcesButton()
+    await historyPage.genaratereport_colored_()
+    await historyPage.closeButton()
+})
+test("_10_Edit Report Title and Verify Reflection on Reports Page",async({page})=>{
+    const {homepage,reportpage}=setup(page)
+    const input="Q1: How are these trends influencing global energy policies?"
 
-//     await homepage.three_vertical_dot_()
-//     await homepage.reportPage_()
-//     await reportpage.first_title_search_results()
-//     await reportpage.editTitle()
-//     await reportpage.updateReportTitle(input)
-//     await reportpage.saveReportTitle()
-//     await reportpage.closeButton()
-//     await reportpage.closeButton()
-// })
-// test("_11_Home Page validation: New Chat, search textfield", async({page})=>{
-//     const {homepage}=setup(page)
-//     const input='How are these trends influencing global energy policies?'
+    await homepage.three_vertical_dot_()
+    await homepage.reportPage_()
+    await reportpage.first_title_search_results()
+    await reportpage.editTitle()
+    await reportpage.updateReportTitle(input)
+    await reportpage.saveReportTitle()
+    await reportpage.closeButton()
+    await reportpage.closeButton()
+})
+test("_11_Home Page validation: New Chat, search textfield", async({page})=>{
+    const {homepage}=setup(page)
+    const input='How are these trends influencing global energy policies?'
 
-//     await homepage.newChatHovering() // mouse hovering
-//     await homepage.searchTextField_(input)
-//     await homepage.searchResults() 
-// })
-// test("_12_Login Page Password Masking Verification",async({page})=>{
-//     const {loginpage,homepage}=setup(page)
+    await homepage.newChatHovering() // mouse hovering
+    await homepage.searchTextField_(input)
+    await homepage.searchResults() 
+})
+test("_12_Login Page Password Masking Verification",async({page})=>{
+    const {loginpage,homepage}=setup(page)
 
-//     await homepage.three_vertical_dot_()
-//     await homepage.logoutButton_()
-//     await loginpage.passwordTextField("pradeep")
-//     const passwordValue=await page.getByLabel(loginpage.password).isVisible()
-//     expect(passwordValue).toBeTruthy()
-// })
-// test("_13_Validate Login Button State Prior to Input Entry",async({page})=>{
-//     const {loginpage,homepage}=setup(page)
-//     await homepage.three_vertical_dot_()
-//     await homepage.logoutButton_()
-//     await loginpage.passwordTextField("pradeep")
-//     await page.getByRole(loginpage.login).isDisabled()
-// })
+    await homepage.three_vertical_dot_()
+    await homepage.logoutButton_()
+    await loginpage.passwordTextField("pradeep")
+    const passwordValue=await page.getByLabel(loginpage.password).isVisible()
+    expect(passwordValue).toBeTruthy()
+})
+test("_13_Validate Login Button State Prior to Input Entry",async({page})=>{
+    const {loginpage,homepage}=setup(page)
+    await homepage.three_vertical_dot_()
+    await homepage.logoutButton_()
+    await loginpage.passwordTextField("pradeep")
+    await page.getByRole(loginpage.login).isDisabled()
+})
 test("Showing Error Message when invalid username and password is entered",async({page})=>{
 
 })
-
 test("Can download, preview, and close HTML files",async({page})=>{
 
 })
@@ -178,27 +174,15 @@ test("Can navigate through objects using horizontal scroll",async({page})=>{
 test("Resources are displayed in a relevant order",async({page})=>{
 
 })
-
 test("Resources are displayed in a relevant order (Filename, Company name, Quater, Rank,Number of matches, Download options)",async({page})=>{
 
 })
-
 test("Each generated chat response summary has like and dislike buttons.Clicking on Like or Dislike results in selection",async({page})=>{
     
 })
-
 test("User can enter his feedback for generated response",async({page})=>{
 
 })
- 
-test("User can Enter multiple queries sequentially",async({page})=>{
+ test("User can Enter multiple queries sequentially",async({page})=>{
 
 })
-
-
-
-
- 
- 
-
- 
